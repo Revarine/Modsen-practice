3a3d48d57d2f762840020bb2efb63d94b64e7bf0
pome1lo <paaworker@gmail.com>
2024-06-19 18:07:49 +0300

    init project, create ui

---
+++ calculator/calculator/calculator/Views/AddFunction.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace calculator.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddFunction.xaml
+    /// </summary>
+    public partial class AddFunction : Window
+    {
+        public AddFunction()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ calculator/calculator/calculator/Views/AddVariable.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace calculator.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddVariable.xaml
+    /// </summary>
+    public partial class AddVariable : Window
+    {
+        public AddVariable()
+        {
+            InitializeComponent();
+        }
+    }
+}
