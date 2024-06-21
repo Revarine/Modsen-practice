3418ec6f2bae303eef8668329d04d0489a16b908
Revarine <sashasakovich9@gmail.com>
2024-06-21 12:57:52 +0300

    Update functions logic and notifications mechanism

--- calculator/calculator/calculator/DataVault/DataVault.cs
+++ calculator/calculator/calculator/DataVault/DataVault.cs
@@ -1,23 +1,124 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace calculator.DataVault
 {
-    public class Variable
+    public class Variable : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public double Value { get; set; }
-    }
-    public class Parameter
-    {
-        public string Name { get; set; }
+        private string name;
+        private double value;
+        
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double Value
+        {
+            get => value;
+            set
+            {
+                if (value != this.value)
+                {
+                    this.value = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public class Function
+    public class Parameter : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Expression { get; set; }
-        public ObservableCollection<Parameter> Parameters { get; set; }
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Function : INotifyPropertyChanged
+    {
+        private string name;
+        private string expression;
+        private ObservableCollection<Parameter> parameters;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Expression
+        {
+            get => expression;
+            set
+            {
+                if (expression != value)
+                {
+                    expression = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<Parameter> Parameters
+        {
+            get => parameters;
+            set
+            {
+                if (parameters != value)
+                {
+                    parameters = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public Function()
         {
