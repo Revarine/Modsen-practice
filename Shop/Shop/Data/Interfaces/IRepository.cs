74a15dcb09aa573be0e7a1e3d640b4c9a043c3f0
pome1lo <paaworker@gmail.com>
2024-06-27 16:07:46 +0300

    Update IRepository, add cancellation token, delete the IDisposable

--- Shop/Shop/Data/Interfaces/IRepository.cs
+++ Shop/Shop/Data/Interfaces/IRepository.cs
@@ -1,12 +1,12 @@
 ﻿namespace Shop.Data.Interfaces
 {
-    public interface IRepository<T> : IDisposable where T : class
+    public interface IRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetIEnumerableAsync();
-        Task<T> GetItemAsync(int id);
-        Task CreateAsync(T item);
-        Task UpdateAsync(T item);
-        Task DeleteAsync(int id);
-        Task SaveAsync();
+        Task<IEnumerable<T>> GetIEnumerableAsync(CancellationToken cancellationToken = default);
+        Task<T> GetItemAsync(int id, CancellationToken cancellationToken = default);
+        Task CreateAsync(T item, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T item, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+        Task SaveAsync(CancellationToken cancellationToken = default);
     }
 }
