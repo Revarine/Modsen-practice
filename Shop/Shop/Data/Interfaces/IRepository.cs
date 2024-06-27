cad4ccc92ec16f7811622ffdd3b7510e04c3376b
pome1lo <paaworker@gmail.com>
2024-06-27 13:45:37 +0300

    Add interface IRepository<T>

---
+++ Shop/Shop/Data/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Data.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetIEnumerable();
+        T GetItem(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+    }
+}
