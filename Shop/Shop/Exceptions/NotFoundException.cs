c52c61dbdaef177897a455d87d886f968828b14f
pome1lo <paaworker@gmail.com>
2024-06-29 22:09:24 +0300

    Add custom exception NotFoundException

---
+++ Shop/Shop/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
