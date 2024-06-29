b23e53669191843d8740fbab374a8bc91cfc0ce4
pome1lo <paaworker@gmail.com>
2024-06-29 18:17:20 +0300

    Add product service interface

---
+++ Shop/Shop/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Services.DTO;
+
+namespace Shop.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductDto> GetProductByIdAsync(int productId);
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId);
+        Task<ProductDto> CreateProductAsync(ProductDto newProduct);
+        Task UpdateProductAsync(int productId, ProductDto updatedProduct);
+        Task DeleteProductAsync(int productId);
+    }
+}
