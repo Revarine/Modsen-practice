64a8ab29bcbcfd36dc5b9200d01bd2a8a367f9d5
pome1lo <paaworker@gmail.com>
2024-06-29 18:15:34 +0300

    Add DTO for product model

---
+++ Shop/Shop/Services/DTO/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Services.DTO
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
