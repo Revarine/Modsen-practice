befe93d6ebddbba576cc5c10fca96bb4f356f577
Revarine <sashasakovich9@gmail.com>
2024-06-30 13:16:05 +0300

    Fix CategoryService interface typos

--- Shop/Shop/Services/Interfaces/ICategoryService.cs
+++ Shop/Shop/Services/Interfaces/ICategoryService.cs
@@ -7,6 +7,6 @@ public interface ICategoryService
     Task<CategoryDto> GetCategoryByIdAsync(int categoryId, CancellationToken cancellationToken = default);
     Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(CancellationToken cancellationToken = default);
     Task<CategoryDto> CreateCategoryAsync(CategoryDto newCategory, CancellationToken cancellationToken = default);
-    Task UpdateCategoryAsync(int productId, CategoryDto updatedCategory, CancellationToken cancellationToken = default);
-    Task DeleteCategoryAsync(int productId, CancellationToken cancellationToken = default);
+    Task UpdateCategoryAsync(int categoryId, CategoryDto updatedCategory, CancellationToken cancellationToken = default);
+    Task DeleteCategoryAsync(int categoryId, CancellationToken cancellationToken = default);
 }
