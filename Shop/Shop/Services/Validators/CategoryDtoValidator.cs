f518ea0044b9af055500dec9570c8d4b3e3146fd
Revarine <sashasakovich9@gmail.com>
2024-06-30 12:29:28 +0300

    Create validator for CategoryDto

---
+++ Shop/Shop/Services/Validators/CategoryDtoValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+using Shop.Services.DTO;
+
+namespace Shop.Services.Validators;
+
+public class CategoryDtoValidator : AbstractValidator<CategoryDto>
+{
+    public CategoryDtoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Category name cannot be empty.");
+    }
+}
