c2c4038b64505df2753a12f838212ae981191d5c
Revarine <sashasakovich9@gmail.com>
2024-07-03 15:38:15 +0300

    Create User Controller. Add get user by id method.

---
+++ Shop/Shop/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Shop.Exceptions;
+using Shop.Services.DTO;
+using Shop.Services.Interfaces;
+
+namespace Shop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUserById(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await _userService.GetUserByIdAsync(id, cancellationToken);
+            return Ok(user);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    
+    
+}
