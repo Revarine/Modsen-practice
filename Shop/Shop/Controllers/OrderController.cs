3db2232416e63e1f4086ce26dbcca62e95abf074
Revarine <sashasakovich9@gmail.com>
2024-07-04 18:42:10 +0300

    Add all actions to all service methods

--- Shop/Shop/Controllers/OrderController.cs
+++ Shop/Shop/Controllers/OrderController.cs
@@ -1,8 +1,85 @@
 using Microsoft.AspNetCore.Mvc;
+using Shop.Exceptions;
+using Shop.Services.DTO;
+using Shop.Services.Interfaces;
 
 namespace Shop.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class OrderController : ControllerBase
 {
-    
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<OrderDto>> GetOrderById(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var order = await _orderService.GetOrderByIdAsync(id, cancellationToken);
+            return Ok(order);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders(CancellationToken cancellationToken = default)
+    {
+        var orders = await _orderService.GetAllOrdersAsync(cancellationToken);
+        return Ok(orders);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<OrderDto>> CreateOrder(OrderDto newOrder, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var createdOrder = await _orderService.CreateOrderAsync(newOrder, cancellationToken);
+            return CreatedAtAction(nameof(CreateOrder), new { id = createdOrder.Id}, createdOrder);
+        }
+        catch (RepeatingNameException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<OrderDto>> UpdateOrder(int id, OrderDto updatedOrder, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _orderService.UpdateOrderAsync(id, updatedOrder, cancellationToken);
+            return NoContent();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (RepeatingNameException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<OrderDto>> DeleteOrder(int id, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _orderService.DeleteOrderAsync(id, cancellationToken);
+            return NoContent();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
 }
