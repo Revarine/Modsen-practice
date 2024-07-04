dea0e8c87c21116a9b08070ba31faf124e08d8db
pome1lo <paaworker@gmail.com>
2024-07-04 15:04:53 +0300

    create global exception handler middleware

---
+++ Shop/Shop/Middlewares/GlobalExceptionHandler/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Shop.Middlewares.GlobalExceptionHandler
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var errorResponse = new
+                {
+                    Message = "An error occurred.",
+                    ErrorCode = "InternalServerError"
+                };
+
+                var result = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
