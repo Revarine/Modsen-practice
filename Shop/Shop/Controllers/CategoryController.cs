1714cb8fd3ed7147785923247a51629af367c3d4
pome1lo <paaworker@gmail.com>
2024-07-05 16:18:37 +0300

    Remove try-catch blocks from controllers

--- Shop/Shop/Controllers/CategoryController.cs
+++ Shop/Shop/Controllers/CategoryController.cs
@@ -28,64 +28,32 @@ namespace Shop.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<CategoryDto>> GetCategoryById(int id, CancellationToken cancellationToken = default)
         {
-            try
-            {
-                var category = await _categoryService.GetCategoryByIdAsync(id, cancellationToken);
-                return Ok(category);
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            var category = await _categoryService.GetCategoryByIdAsync(id, cancellationToken);
+            return Ok(category);
         }
 
         // POST: api/Categories
         [HttpPost]
         public async Task<ActionResult<CategoryDto>> CreateCategory(CategoryDto newCategory, CancellationToken cancellationToken = default)
         {
-            try
-            {
-                var createdCategory = await _categoryService.CreateCategoryAsync(newCategory, cancellationToken);
-                return CreatedAtAction(nameof(GetCategoryById), new { id = createdCategory.Id }, createdCategory);
-            }
-            catch (RepeatingNameException ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            var createdCategory = await _categoryService.CreateCategoryAsync(newCategory, cancellationToken);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = createdCategory.Id }, createdCategory);
         }
 
         // PUT: api/Categories/5
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateCategory(int id, CategoryDto updatedCategory, CancellationToken cancellationToken = default)
         {
-            try
-            {
-                await _categoryService.UpdateCategoryAsync(id, updatedCategory, cancellationToken);
-                return NoContent();
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (RepeatingNameException ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            await _categoryService.UpdateCategoryAsync(id, updatedCategory, cancellationToken);
+            return NoContent();
         }
 
         // DELETE: api/Categories/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(int id, CancellationToken cancellationToken = default)
         {
-            try
-            {
-                await _categoryService.DeleteCategoryAsync(id, cancellationToken);
-                return NoContent();
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
+            await _categoryService.DeleteCategoryAsync(id, cancellationToken);
+            return NoContent();
         }
     }
 }
