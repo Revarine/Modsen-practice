20c785f4cace144605304727fe60deb64800f20b
pome1lo <paaworker@gmail.com>
2024-07-07 12:59:40 +0300

    move models

---
+++ Shop/DataAccess/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+namespace DataAccess.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
